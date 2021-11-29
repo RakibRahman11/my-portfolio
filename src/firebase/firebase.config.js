8bb9fc2f408319d22b83a000830ede42a4801b88
RakibRahman11 <rahmanrakib207@gmail.com>
2021-11-29 20:53:36 +0600

    deploy

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCVKhdE4YoNf0VvRC3WSzlCDP8rCGoAKqc",
+    authDomain: "my-portfolio-11.firebaseapp.com",
+    projectId: "my-portfolio-11",
+    storageBucket: "my-portfolio-11.appspot.com",
+    messagingSenderId: "1054252782735",
+    appId: "1:1054252782735:web:39ee531a9b9805067b0cfa"
+};
+export default firebaseConfig;
